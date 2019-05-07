703890295967cc7dc1c2fdbecef51d4fe1768b76
Giovana Morais <giovana.vmorais@gmail.com>
2019-05-07 13:54:02 -0300

    correção de link quebrado e inserção de bibliotecas faltantes

--- app/src/main/java/io/github/giovana_morais/intergeracoes/MainActivity.java
+++ app/src/main/java/io/github/giovana_morais/intergeracoes/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.FirebaseApp;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         aulas.add(new VideoAula("Whatsapp", "Iniciar conversa", "gs://intergeracoes-2018.appspot.com/wpp_iniciar_conversa.mp4"));
         aulas.add(new VideoAula("Whatsapp", "Deletar mensagem", "gs://intergeracoes-2018.appspot.com/wpp_deletar_mensagem"));
         aulas.add(new VideoAula("Whatsapp", "Editar status", "gs://intergeracoes-2018.appspot.com/wpp_editar_status.mp4"));
-        aulas.add(new VideoAula("Whataspp", "Encaminhar mensagem", "gs://intergeracoes-2018.appspot.com/wpp_encaminhar_mensagem"));
+        aulas.add(new VideoAula("Whataspp", "Encaminhar mensagem", "gs://intergeracoes-2018.appspot.com/wpp_encaminhar_mensagem.mp4"));
         aulas.add(new VideoAula("Whataspp", "Enviar áudio", "gs://intergeracoes-2018.appspot.com/wpp_enviar_audio.mp4"));
         aulas.add(new VideoAula("Whataspp", "Enviar emoji", "gs://intergeracoes-2018.appspot.com/wpp_enviar_emoji.mp4"));
         aulas.add(new VideoAula("Whataspp", "Trocar foto de perfil", "gs://intergeracoes-2018.appspot.com/wpp_foto_perfil.mp4"));
@@ -77,8 +80,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             if (files != null) {
                 for (File f : files) {
                     if (f.isDirectory()) {
+                        Log.i("Sustenta", "Deletando diretório " + file.getName());
                         deleteTempFiles(f);
                     } else {
+                        Log.i("Sustenta", "Deletando arquivo " + file.getName());
                         f.delete();
                     }
                 }
